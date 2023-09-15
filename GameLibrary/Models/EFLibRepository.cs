d202d7626b54cc173156e4ad869f56745922b3e7
lomiklomi <55328668+lomiklomi@users.noreply.github.com>
2023-09-15 21:15:19 +0500

    ILibRepository implementation

---
+++ GameLibrary/Models/EFLibRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace GameLibrary.Models
+{
+	public class EFLibRepository : ILibRepository
+	{
+		private LibDbContext context;
+		public EFLibRepository(LibDbContext ctx)
+		{
+			context = ctx;
+		}
+		public IQueryable<Game> Games => context.Games;
+	}
+}

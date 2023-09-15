f2539d857af469b7381394d697b6a411ac761c68
lomiklomi <55328668+lomiklomi@users.noreply.github.com>
2023-09-15 21:19:55 +0500

    LibDbContext for saving instance Games

---
+++ GameLibrary/Models/LibDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace GameLibrary.Models
+{
+	public class LibDbContext : DbContext
+	{
+		public LibDbContext(DbContextOptions<LibDbContext> options)
+			: base(options) { }
+		public DbSet<Game> Games => Set<Game>();
+	}
+}

056260bb6f0520907043d7434dea472be70622f0
lomiklomi <55328668+lomiklomi@users.noreply.github.com>
2023-09-15 21:27:23 +0500

    + libraries + Connection to DB + Migration

--- GameLibrary/Program.cs
+++ GameLibrary/Program.cs
@@ -1,13 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using GameLibrary.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<LibDbContext>(opts => {
+	opts.UseSqlServer(
+		builder.Configuration["ConnectionStrings:GameLibConnection"]);
+});
+
+builder.Services.AddScoped<ILibRepository, EFLibRepository>();
+
 var app = builder.Build();
-
-
-//app.MapGet("/", () => "Hello World!");
 
 app.UseStaticFiles();
 app.MapDefaultControllerRoute();
+
+SeedData.EnsurePopulated(app);
 
 app.Run();

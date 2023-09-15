8b64361fbc8d3cbf321698eead7c420e5341a36f
lomiklomi <55328668+lomiklomi@users.noreply.github.com>
2023-09-15 21:17:14 +0500

    Remove unused methods

--- GameLibrary/Models/Game.cs
+++ GameLibrary/Models/Game.cs
@@ -1,13 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
-namespace SportsStore.Models
+namespace GameLibrary.Models
 {
 	public class Game
 	{
 		public long? GameID { get; set; }
 		public string Name { get; set; } = String.Empty;
-		public string Description { get; set; } = String.Empty;
-		[Column(TypeName = "decimal(8, 2)")]
-		public decimal Price { get; set; }
-		public string Category { get; set; } = String.Empty;
+		public string Developer { get; set; } = String.Empty;
 	}
 }

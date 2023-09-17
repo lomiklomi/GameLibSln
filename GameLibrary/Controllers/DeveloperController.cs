452bbba845fecaf512f0e2efceb17952cfd52fb9
lomiklomi <55328668+lomiklomi@users.noreply.github.com>
2023-09-18 02:29:18 +0500

    Add DeveloperController

---
+++ GameLibrary/Controllers/DeveloperController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GameLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace GameLibrary.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class DeveloperController : ControllerBase
+	{
+		private LibDbContext context;
+		public DeveloperController(LibDbContext ctx)
+		{
+			context = ctx;
+		}
+		[HttpGet("{id}")]
+		public async Task<Developer?> GetDeveloper(long id)
+		{
+			Developer developer = await context.Developer.Include(s => s.Games)
+				.FirstAsync(s => s.DeveloperId == id);
+			if (developer.Games != null)
+			{
+				foreach (Game p in developer.Games)
+				{
+					p.Developer = null;
+				};
+			}
+			return developer;
+		}
+
+		[HttpPatch("{id}")]
+		public async Task<Developer?> PatchDeveloper(long id,
+			JsonPatchDocument<Developer> patchDoc)
+		{
+			Developer? s = await context.Developer.FindAsync(id);
+			if (s != null)
+			{
+				patchDoc.ApplyTo(s);
+				await context.SaveChangesAsync();
+			}
+			return s;
+		}
+	}
+}

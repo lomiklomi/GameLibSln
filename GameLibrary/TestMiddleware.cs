ca2f1ef26c0fd72a0b1bd41f186df3b64e8ca492
lomiklomi <55328668+lomiklomi@users.noreply.github.com>
2023-09-18 02:40:46 +0500

    Test

---
+++ GameLibrary/TestMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using GameLibrary.Models;
+
+namespace GameLibrary
+{
+	public class TestMiddleware
+	{
+		private RequestDelegate nextDelegate;
+		public TestMiddleware(RequestDelegate next)
+		{
+			nextDelegate = next;
+		}
+		public async Task Invoke(HttpContext context, LibDbContext dataContext)
+		{
+			if (context.Request.Path == "/test")
+			{
+				await context.Response.WriteAsync(
+				$"There are {dataContext.Game.Count()} products\n");
+				await context.Response.WriteAsync(
+				$"There are {dataContext.Genre.Count()} categories\n");
+				await context.Response.WriteAsync(
+				$"There are {dataContext.Developer.Count()} suppliers\n");
+			}
+			else
+			{
+				await nextDelegate(context);
+			}
+		}
+	}
+}

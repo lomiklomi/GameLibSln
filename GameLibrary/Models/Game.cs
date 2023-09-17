a56a5d45225b54d66c15aeaf8aea4cbf6852af5f
lomiklomi <55328668+lomiklomi@users.noreply.github.com>
2023-09-18 02:32:48 +0500

    GameId null. Dev && Genre notNull

--- GameLibrary/Models/Game.cs
+++ GameLibrary/Models/Game.cs
@@ -5,14 +5,14 @@ namespace GameLibrary.Models
 {
 	public class Game
 	{
-		public long? GameId { get; set; }
+		public long GameId { get; set; }
 		public string Name { get; set; } = String.Empty;
 
 		public long DeveloperId { get; set; }
 		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-		public Developer Developer { get; set; } = new Developer();
+		public Developer? Developer { get; set; } = new Developer();
 
 		public long GenreId { get; set; }
-		public Genre Genre { get; set; } = new Genre();
+		public Genre? Genre { get; set; } = new Genre();
 	}
 }

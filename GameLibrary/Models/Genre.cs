f5cc60b9cd63734c05694518c66ff4df5328fb2b
lomiklomi <55328668+lomiklomi@users.noreply.github.com>
2023-09-18 02:33:36 +0500

    GameId can be null

--- GameLibrary/Models/Genre.cs
+++ GameLibrary/Models/Genre.cs
@@ -2,7 +2,7 @@
 {
 	public class Genre
 	{
-		public long? GenreId { get; set; }
+		public long GenreId { get; set; }
 		public string Name { get; set; } = String.Empty;
 		public IEnumerable<Game>? Games { get; set; }
 	}

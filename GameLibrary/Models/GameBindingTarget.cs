b02d82ca76eba2798621bb247c55e7bafd631140
lomiklomi <55328668+lomiklomi@users.noreply.github.com>
2023-09-18 02:33:11 +0500

    Add GameBIndingTarget

---
+++ GameLibrary/Models/GameBindingTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameLibrary.Models
+{
+	public class GameBindingTarget
+	{
+		[Required]
+		public string Name { get; set; } = "";
+		[Range(1, long.MaxValue)]
+		public long GenreId { get; set; }
+		[Range(1, long.MaxValue)]
+		public long DeveloperId { get; set; }
+		public Game ToGame() => new Game()
+		{
+			Name = this.Name,
+			GenreId = this.GenreId,
+			DeveloperId = this.DeveloperId
+		};
+	}
+}

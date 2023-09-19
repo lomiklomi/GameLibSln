2f11fb89393c443ae9f11cb25cb56edc26dee503
lomiklomi <55328668+lomiklomi@users.noreply.github.com>
2023-09-19 07:39:24 +0500

    New genreController

---
+++ GameLibrary/Controllers/GenreController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GameLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace GameLibrary.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class GenreController : ControllerBase
+	{
+		private LibDbContext context;
+		public GenreController(LibDbContext ctx)
+		{
+			context = ctx;
+		}
+		[HttpGet("{id}")]
+		public async Task<Genre?> GetGenre(long id)
+		{
+			Genre genre = await context.Genre.Include(s => s.Games)
+				.FirstAsync(s => s.GenreId == id);
+			if (genre.Games != null)
+			{
+				foreach (Game p in genre.Games)
+				{
+					p.Genre = null;
+				};
+			}
+			return genre;
+		}
+
+		[HttpPatch("{id}")]
+		public async Task<Genre?> PatchGenre(long id,
+			JsonPatchDocument<Genre> patchDoc)
+		{
+			Genre? s = await context.Genre.FindAsync(id);
+			if (s != null)
+			{
+				patchDoc.ApplyTo(s);
+				await context.SaveChangesAsync();
+			}
+			return s;
+		}
+	}
+}

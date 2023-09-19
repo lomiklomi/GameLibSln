e6bfedd3ae421412ffa4a47aafedc9e35371c1ae
lomiklomi <55328668+lomiklomi@users.noreply.github.com>
2023-09-19 07:42:35 +0500

    Remove interface and implementation of context

--- GameLibrary/Models/ILibRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GameLibrary.Models
-{
-	public interface ILibRepository
-	{
-		IQueryable<Game> Game { get; }
-		IQueryable<Genre> Genre { get; }
-		IQueryable<Developer> Developer { get; }
-	}
-}
